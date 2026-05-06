af7b8e6ec8ea8abd6cabe73e2b38d7d36bcf2d2f
Minh Nguyen <mnguyen6@unb.ca>
2026-05-06 11:39:04 -0300

    [dev_docker] fix timezone mismatch and modify ros_entrypoint
    changes to ros_entrypoint.sh
    - added the ROS2 underlay and X3_ROS2_ws overlay sourcing
    - added git submodule update and init for the rplidar_ros and any future submodules

--- .devcontainer/ros_entrypoint.sh
+++ .devcontainer/ros_entrypoint.sh
@@ -11,4 +11,14 @@ echo "----------"
 echo -e "ROS_DOMAIN_ID:\033[32m$ROS_DOMAIN_ID\033[0m | ROS_IP:\033[32m$ROS_IP\033[0m"
 echo -e "LIDAR_TYPE: \033[32m$LIDAR_TYPE\033[0m"
 echo "----------"
+
+source /opt/ros/humble/setup.bash
+
+# Initialize submodules if not already done 
+#   using rplidar_ros as submodule requires cloning with --recursive flag, if this was not 
+#   done then the following command updates the submodule and make it available for use
+git submodule update --init --recursive
+
+source /X3_ROS2_ws/install/setup.bash
+
 exec "$@"
