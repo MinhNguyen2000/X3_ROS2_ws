805ad3e66a48ddb7698528e4ea1577a0de80a48e
Minh Nguyen <mnguyen6@unb.ca>
2026-08-06 12:42:45 -0300

    [x3_description/drl_policy/apf_planner] rotate the lidar visual instead of the joint since it had negative impact on the rf2o scan matching

--- src/x3_drl_policy/x3_drl_policy/policy_node.py
+++ src/x3_drl_policy/x3_drl_policy/policy_node.py
@@ -178,14 +178,14 @@ class DRLPolicyNode(Node):
     def lidar_callback(self, msg: LaserScan):
         ranges = np.array(msg.ranges)
 
-        # # index of the zero angle in the (-π, π) scan
-        # zero_idx = round(-msg.angle_min / msg.angle_increment)
+        # index of the zero angle in the (-π, π) scan
+        zero_idx = round(-msg.angle_min / msg.angle_increment)
 
-        # # rotate the ranges from (-π, π) to (0, 2π)
-        # ranges_drl = np.roll(ranges, -zero_idx)
+        # rotate the ranges from (-π, π) to (0, 2π)
+        ranges_drl = np.roll(ranges, -zero_idx)
 
         self.latest_scan = copy.deepcopy(msg)
-        # self.latest_scan.ranges = ranges_drl.tolist()
+        self.latest_scan.ranges = ranges_drl.tolist()
 
     def goal_callback(self, goal_request: NavigateToGoal):
         '''
